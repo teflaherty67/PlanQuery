926eb4c8b7b64ad416f898c501dfaf871dba8e13
Tom E Flaherty <67797311+teflaherty67@users.noreply.github.com>
2026-02-16 20:53:03 -0600

    ready to test

--- PlanQuery/Common/Utils.cs
+++ PlanQuery/Common/Utils.cs
@@ -5,49 +5,17 @@ namespace PlanQuery.Common
 {
     internal static class Utils
     {
-        internal static RibbonPanel CreateRibbonPanel(UIControlledApplication app, string tabName, string panelName)
-        {
-            RibbonPanel curPanel;
-
-            if (GetRibbonPanelByName(app, tabName, panelName) == null)
-                curPanel = app.CreateRibbonPanel(tabName, panelName);
-
-            else
-                curPanel = GetRibbonPanelByName(app, tabName, panelName);
-
-            return curPanel;
-        }
-
-        internal static RibbonPanel GetRibbonPanelByName(UIControlledApplication app, string tabName, string panelName)
-        {
-            foreach (RibbonPanel tmpPanel in app.GetRibbonPanels(tabName))
-            {
-                if (tmpPanel.Name == panelName)
-                    return tmpPanel;
-            }
-
-            return null;
-        }
-
         #region Parameters
 
-        internal static string GetParameterValueByName(Element element, string paramName)
+        public struct ParameterData
         {
-            IList<Parameter> paramList = element.GetParameters(paramName);
-
-            if (paramList != null)
-                try
-                {
-                    Parameter param = paramList[0];
-                    string paramValue = param.AsValueString();
-                    return paramValue;
-                }
-                catch (System.ArgumentOutOfRangeException)
-                {
-                    return null;
-                }
-
-            return "";
+            public Definition def;
+            public ElementBinding binding;
+            public string name;
+            public bool IsSharedStatusKnown;
+            public bool IsShared;
+            public string GUID;
+            public ElementId id;
         }
 
         public static List<ParameterData> GetAllProjectParameters(Document curDoc)
@@ -86,6 +54,282 @@ namespace PlanQuery.Common
                 }
             }
             return false;
+        }
+
+        public static void CreateSharedParam(Document curDoc, string groupName, string paramName, BuiltInCategory cat)
+        {
+            Definition curDef = null;
+
+            //check if current file has shared param file - if not then exit
+            DefinitionFile defFile = curDoc.Application.OpenSharedParameterFile();
+
+            //check if file has shared parameter file
+            if (defFile == null)
+            {
+                TaskDialog.Show("Error", "No shared parameter file.");
+                //Throw New Exception("No Shared Parameter File!")
+            }
+
+            //check if shared parameter exists in shared param file - if not then create
+            if (ParamExists(defFile.Groups, groupName, paramName) == false)
+            {
+                //create param
+                curDef = AddParamToFile(defFile, groupName, paramName);
+            }
+            else
+            {
+                curDef = GetParameterDefinitionFromFile(defFile, groupName, paramName);
+            }
+
+            //check if param is added to views - if not then add
+            if (ParamAddedToFile(curDoc, paramName) == false)
+            {
+                //add parameter to current Revitfile
+                AddParamToDocument(curDoc, curDef, cat);
+            }
+        }
+
+        internal static string GetParameterValueByName(Element element, string paramName)
+        {
+            IList<Parameter> paramList = element.GetParameters(paramName);
+
+            if (paramList != null)
+                try
+                {
+                    Parameter param = paramList[0];
+                    string paramValue = param.AsValueString();
+                    return paramValue;
+                }
+                catch (System.ArgumentOutOfRangeException)
+                {
+                    return null;
+                }
+
+            return "";
+        }
+
+        internal static Parameter GetParameterByName(Element curElem, string paramName)
+        {
+            foreach (Parameter curParam in curElem.Parameters)
+            {
+                if (curParam.Definition.Name.ToString() == paramName)
+                    return curParam;
+            }
+
+            return null;
+        }
+
+        internal static Parameter GetParameterByNameAndWritable(Element curElem, string paramName)
+        {
+            foreach (Parameter curParam in curElem.Parameters)
+            {
+                if (curParam.Definition.Name.ToString() == paramName && curParam.IsReadOnly == false)
+                    return curParam;
+            }
+
+            return null;
+        }
+
+        internal static ElementId GetProjectParameterId(Document curDoc, string name)
+        {
+            ParameterElement pElem = new FilteredElementCollector(curDoc)
+                .OfClass(typeof(ParameterElement))
+                .Cast<ParameterElement>()
+                .Where(e => e.Name.Equals(name))
+                .FirstOrDefault();
+
+            return pElem?.Id;
+        }
+
+        internal static ElementId GetBuiltInParameterId(Document curDoc, BuiltInCategory cat, BuiltInParameter bip)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(curDoc);
+            collector.OfCategory(cat);
+
+            Parameter curParam = collector.FirstElement().get_Parameter(bip);
+
+            return curParam?.Id;
+        }
+
+        internal static string SetParameterByNameAndWritable(Element curElem, string paramName, string value)
+        {
+            Parameter curParam = GetParameterByNameAndWritable(curElem, paramName);
+
+            curParam.Set(value);
+            return curParam.ToString();
+        }
+
+        internal static void SetParameterByName(Element element, string paramName, string value)
+        {
+            IList<Parameter> paramList = element.GetParameters(paramName);
+
+            if (paramList != null)
+            {
+                Parameter param = paramList[0];
+
+                param.Set(value);
+            }
+        }
+
+        internal static void SetParameterByName(Element element, string paramName, int value)
+        {
+            IList<Parameter> paramList = element.GetParameters(paramName);
+
+            if (paramList != null)
+            {
+                Parameter param = paramList[0];
+
+                param.Set(value);
+            }
+        }
+
+        internal static bool SetParameterValue(Element curElem, string paramName, string value)
+        {
+            Parameter curParam = GetParameterByName(curElem, paramName);
+
+            if (curParam != null)
+            {
+                curParam.Set(value);
+                return true;
+            }
+
+            return false;
+        }
+
+        internal static Definition GetParameterDefinitionFromFile(DefinitionFile defFile, string groupName, string paramName)
+        {
+            // iterate the Definition groups of this file
+            foreach (DefinitionGroup group in defFile.Groups)
+            {
+                if (group.Name == groupName)
+                {
+                    // iterate the difinitions
+                    foreach (Definition definition in group.Definitions)
+                    {
+                        if (definition.Name == paramName)
+                            return definition;
+                    }
+                }
+            }
+            return null;
+        }
+
+        //check if specified parameter is already added to Revit file
+        public static bool ParamAddedToFile(Document curDoc, string paramName)
+        {
+            foreach (Parameter curParam in curDoc.ProjectInformation.Parameters)
+            {
+                if (curParam.Definition.Name.Equals(paramName))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static bool AddParamToDocument(Document curDoc, Definition curDef, BuiltInCategory cat)
+        {
+            bool paramAdded = false;
+
+            //define category for shared param
+            Category myCat = curDoc.Settings.Categories.get_Item(cat);
+            CategorySet myCatSet = curDoc.Application.Create.NewCategorySet();
+            myCatSet.Insert(myCat);
+
+            //create binding
+            ElementBinding curBinding = curDoc.Application.Create.NewInstanceBinding(myCatSet);
+
+            //do something
+            //paramAdded = curDoc.ParameterBindings.Insert(curDef, curBinding, BuiltInParameterGroup.PG_IDENTITY_DATA);
+
+            return paramAdded;
+        }
+
+
+        //check if specified parameter exists in shared parameter file
+        public static bool ParamExists(DefinitionGroups groupList, string groupName, string paramName)
+        {
+            //loop through groups and look for match
+            foreach (DefinitionGroup curGroup in groupList)
+            {
+                if (curGroup.Name.Equals(groupName) == true)
+                {
+                    //check if param exists
+                    foreach (Definition curDef in curGroup.Definitions)
+                    {
+                        if (curDef.Name.Equals(paramName))
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        //add parameter to specified shared parameter file
+        public static Definition AddParamToFile(DefinitionFile defFile, string groupName, string paramName)
+        {
+            //create new shared parameter in specified file
+            DefinitionGroup defGroup = GetDefinitionGroup(defFile, groupName);
+
+            //check if group exists - if not then create
+            if (defGroup == null)
+            {
+                //create group
+                defGroup = defFile.Groups.Create(groupName);
+            }
+
+            //create parameter in group
+            ExternalDefinitionCreationOptions curOptions = new ExternalDefinitionCreationOptions(paramName, SpecTypeId.String.Text);
+            curOptions.Visible = true;
+
+            Definition newParam = defGroup.Definitions.Create(curOptions);
+
+            return newParam;
+        }
+
+        public static DefinitionGroup GetDefinitionGroup(DefinitionFile defFile, string groupName)
+        {
+            //loop through groups and look for match
+            foreach (DefinitionGroup curGroup in defFile.Groups)
+            {
+                if (curGroup.Name.Equals(groupName))
+                {
+                    return curGroup;
+                }
+            }
+
+            return null;
+        }
+
+        #endregion
+
+        #region Ribbon
+        internal static RibbonPanel CreateRibbonPanel(UIControlledApplication app, string tabName, string panelName)
+        {
+            RibbonPanel curPanel;
+
+            if (GetRibbonPanelByName(app, tabName, panelName) == null)
+                curPanel = app.CreateRibbonPanel(tabName, panelName);
+
+            else
+                curPanel = GetRibbonPanelByName(app, tabName, panelName);
+
+            return curPanel;
+        }
+
+        internal static RibbonPanel GetRibbonPanelByName(UIControlledApplication app, string tabName, string panelName)
+        {
+            foreach (RibbonPanel tmpPanel in app.GetRibbonPanels(tabName))
+            {
+                if (tmpPanel.Name == panelName)
+                    return tmpPanel;
+            }
+
+            return null;
         }
 
         #endregion
--- PlanQuery/cmdAddParameters.cs
+++ PlanQuery/cmdAddParameters.cs
@@ -12,14 +12,14 @@ namespace PlanQuery
             UIDocument uidoc = uiapp.ActiveUIDocument;
             Document curDoc = uidoc.Document;
 
-            // set variable for the shared parameter file path
+            // set variable for the shared parameter file path and group name
             string sharedParamFile = @"S:\Shared Folders\Lifestyle USA Design\Library 2026\LD_Shared-Parameters_Master.txt";
+            string sharedParamGroup = "Project Information";
 
             // define list of parameters to add
             List<string> listParams = new List<string> { "Spec Level", "Client Division", "Client Subdivision", "Garage Loading" };
 
-
-            // add the parameters to the project file
+            // wrap operation in a try-catch block to handle any errors that may occur
             try
             {
                 // check if the shared parameter file exists
@@ -37,7 +37,7 @@ namespace PlanQuery
                 // loop through the list of parameters and sort them into the added and existing lists
                 foreach (string curParam in listParams)
                 {
-                    if (Utils.DoesParameterExists(curDoc, curParam))
+                    if (Utils.DoesProjectParamExist(curDoc, curParam))
                     {
                         existingParams.Add(curParam);
                     }
@@ -47,22 +47,88 @@ namespace PlanQuery
                     }
                 }
 
+                // if all parameters already exist in the project, notify user and exit
+                if (addedParams.Count == 0)
+                {
+                   Utils.TaskDialogInformation("Plan Query", "Parameters Exist",
+                        $"All parameters already exist in the project:\n{string.Join("\n", existingParams)}");
+                    return Result.Succeeded;
+                }
 
+                // set the shared parameter file and open it
+                uiapp.Application.SharedParametersFilename = sharedParamFile;
 
+                // set the definition file variable to the currently open shared parameter file
+                DefinitionFile curDefFile = uiapp.Application.OpenSharedParameterFile();
 
+                // null check the definition file
+                if (curDefFile == null)
+                {
+                    Utils.TaskDialogError("Plan Query", "Error",
+                        $"Could not open shared parameter file:\n{sharedParamFile}");
+                    return Result.Failed;
+                }
 
+                // set up binding to the Project Information category
+                CategorySet catSet = new CategorySet();
+                Category catProjInfo = curDoc.Settings.Categories.get_Item(BuiltInCategory.OST_ProjectInformation);
+                catSet.Insert(catProjInfo);
+                InstanceBinding instBinding = uiapp.Application.Create.NewInstanceBinding(catSet);
 
+                // create a transaction to add the parameters to the project
+                using (Transaction t = new Transaction(curDoc, "Add Shared Parameters"))
+                {
+                    // start the transaction
+                    t.Start();
 
+                    // loop through the list of parameters to add
+                    foreach (string curParamName in addedParams)
+                    {
+                        // get the parameter definition from the shared parameter file
+                        Definition curDef = Utils.GetParameterDefinitionFromFile(curDefFile, sharedParamGroup, curParamName);
 
+                        // null check the parameter definition
+                        if (curDef == null)
+                        {
+                            // notify the user if the parameter definition could not be found
+                            Utils.TaskDialogError("Plan Query", "Error",
+                                $"Could not find definition for parameter '{curParamName}' in shared parameter file:\n{sharedParamFile}");
+
+                            // skip to the next parameter in the list
+                            continue;
+                        }
+
+                        // bind the parameter under "Other"
+                        curDoc.ParameterBindings.Insert(curDef, instBinding);
+                    }
+
+                    // commit the transaction
+                    t.Commit();
+                }
+
+                // build the result message
+                string resultMessage = $"Added {addedParams.Count} parameter(s):\n";
+                foreach (string name in addedParams)
+                    resultMessage += $"  - {name}\n";
+
+                if (existingParams.Count > 0)
+                {
+                    resultMessage += $"\n{existingParams.Count} parameter(s) already exist in the project and were not added:\n";
+                    foreach (string name in existingParams)
+                        resultMessage += $"  - {name}\n";
+                }
+
+                Utils.TaskDialogInformation("Plan Query", "Parameters Added", resultMessage);
+
+                return Result.Succeeded;
             }
+
             catch (Exception ex)
             {
-                message = $"Error adding parameters: {ex.Message}";
+                message = ex.Message;
+                Utils.TaskDialogError("Plan Query", "Error", $"An error occurred:\n{ex.Message}");
                 return Result.Failed;
             }
-
-
-            return Result.Succeeded;
         }
         internal static PushButtonData GetButtonData()
         {
